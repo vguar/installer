dcda387475b3eccf87a8335f08df628f8c453d31
Stephane Erbrech <stephane.erbrech@gmail.com>
2019-03-21 15:38:26 -0700

    add public and private dns zones, internal-lb

--- data/data/azure/dns/dns.tf
+++ data/data/azure/dns/dns.tf
@@ -1,7 +1,8 @@
 resource "azurerm_dns_zone" "private" {
-  name                = "${var.cluster_domain}"
+  name                = "${var.base_domain}"
   resource_group_name = "${var.resource_group_name}"
   zone_type           = "Private"
+  resolution_virtual_network_ids = ["${var.internal_dns_resolution_vnet_id}"]
 }
 
 resource "azurerm_dns_zone" "public" {
@@ -10,18 +11,18 @@ resource "azurerm_dns_zone" "public" {
   zone_type           = "Public"
 }
 
-resource "azurerm_dns_cname_record" "api_internal" {
-  name                = "api"
+resource "azurerm_dns_a_record" "api_internal" {
+  name                = "api.${var.cluster_name}"
   zone_name           = "${azurerm_dns_zone.private.name}"
   resource_group_name = "${var.resource_group_name}"
   ttl                 = 300
-  record              = "${var.api_internal_lb_dns_name}"
+  records             = ["${var.internal_lb_ipaddress}"]
 }
 
-resource "azurerm_dns_a_record" "api_external" {
+resource "azurerm_dns_cname_record" "api_external" {
   name                = "api"
   zone_name           = "${azurerm_dns_zone.public.name}"
   resource_group_name = "${var.resource_group_name}"
   ttl                 = 300
-  record              = "${var.api_external_lb_dns_name}"
+  record              = "${var.external_lb_dns_label}.${var.base_domain}"
 }
--- data/data/azure/dns/variables.tf
+++ data/data/azure/dns/variables.tf
@@ -9,13 +9,28 @@ variable "cluster_domain" {
   type        = "string"
 }
 
-variable "api_external_lb_dns_name" {
+variable "cluster_name" {
+  description = "The name for the cluster without the random suffix"
+  type        = "string"
+}
+
+variable "base_domain" {
+  description = "The domain for the cluster that all DNS records must belong"
+  type        = "string"
+}
+
+variable "external_lb_dns_label" {
   description = "External API's LB DNS name"
   type        = "string"
 }
 
-variable "api_internal_lb_dns_name" {
-  description = "External API's LB DNS name"
+variable "internal_lb_ipaddress" {
+  description = "External API's LB Ip address"
+  type        = "string"
+}
+
+variable "internal_dns_resolution_vnet_id" {
+  description = "the vnet id to be attached to the private DNS zone"
   type        = "string"
 }
 
