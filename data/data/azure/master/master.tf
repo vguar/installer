12729d2c9f310a8c84af69c077aa3b419c284089
Stephane Erbrech <stephane.erbrech@gmail.com>
2019-03-29 13:08:16 -0700

    set os disk name using count.index

--- data/data/azure/master/master.tf
+++ data/data/azure/master/master.tf
@@ -65,7 +65,7 @@ resource "azurerm_virtual_machine" "master" {
   delete_os_disk_on_termination = true
 
   storage_os_disk {
-    #name              = "masterosdisk${count.index}"
+    name              = "masterosdisk${count.index}"
     managed_disk_type = "Standard_LRS"
     caching           = "ReadWrite"
     create_option     = "FromImage"
