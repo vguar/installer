051d97b507ee5693ff905822c909ba68c0334284
Stephane Erbrech <stephane.erbrech@gmail.com>
2019-03-29 15:29:15 -0700

    review:s/AWS/Azure

--- pkg/types/azure/platform.go
+++ pkg/types/azure/platform.go
@@ -3,15 +3,15 @@ package azure
 // Platform stores all the global configuration that all machinesets
 // use.
 type Platform struct {
-	// Region specifies the AWS region where the cluster will be created.
+	// Region specifies the Azure region where the cluster will be created.
 	Region string `json:"region"`
 
-	// UserTags specifies additional tags for AWS resources created for the cluster.
+	// UserTags specifies additional tags for Azure resources created for the cluster.
 	// +optional
 	UserTags map[string]string `json:"userTags,omitempty"`
 
 	// DefaultMachinePlatform is the default configuration used when
-	// installing on AWS for machine pools which do not define their own
+	// installing on Azure for machine pools which do not define their own
 	// platform configuration.
 	// +optional
 	DefaultMachinePlatform *MachinePool `json:"defaultMachinePlatform,omitempty"`
