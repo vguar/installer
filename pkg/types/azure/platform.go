42e4ba7da0b86f2086c98afbcfc6578c90d5e7e4
Stephane Erbrech <stephane.erbrech@gmail.com>
2019-04-02 21:25:05 -0700

    feat: querying for public dns zone

--- pkg/types/azure/platform.go
+++ pkg/types/azure/platform.go
@@ -1,10 +1,15 @@
 package azure
 
+import "strings"
+
 // Platform stores all the global configuration that all machinesets
 // use.
 type Platform struct {
 	// Region specifies the Azure region where the cluster will be created.
 	Region string `json:"region"`
+
+	// BaseDomainResourceGroupName specifies the resource group where the azure DNS zone for the base domain is found
+	BaseDomainResourceGroupName string `json:"BaseDomainResourceGroupName,omitempty"`
 
 	// UserTags specifies additional tags for Azure resources created for the cluster.
 	// +optional
@@ -15,4 +20,11 @@ type Platform struct {
 	// platform configuration.
 	// +optional
 	DefaultMachinePlatform *MachinePool `json:"defaultMachinePlatform,omitempty"`
+}
+
+//SetBaseDomain parses the baseDomainID and sets the related fields on azure.Platform
+func (p *Platform) SetBaseDomain(baseDomainID string) error {
+	parts := strings.Split(baseDomainID, "/")
+	p.BaseDomainResourceGroupName = parts[4]
+	return nil
 }
