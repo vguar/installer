0a176566ee3296b5e67503bf1dc5541f60e41c7d
Stephane Erbrech <stephane.erbrech@gmail.com>
2019-05-09 10:00:45 -0700

    data/data/azure: add ssh inbound for debugging
    this can either be reverted later
    or disabled and put behind a feature/option flag

--- data/data/azure/vnet/nsg.tf
+++ data/data/azure/vnet/nsg.tf
@@ -19,3 +19,17 @@ resource "azurerm_subnet_network_security_group_association" "worker" {
   subnet_id                 = "${azurerm_subnet.node_subnet.id}"
   network_security_group_id = "${azurerm_network_security_group.worker.id}"
 }
+
+resource "azurerm_network_security_rule" "ssh_in" {
+  name                        = "ssh_in"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "2200-2204"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "${var.resource_group_name}"
+  network_security_group_name = "${azurerm_network_security_group.master.name}"
+}
